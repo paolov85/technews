5d3fdf8f1e436ccafa3059b63e4045085d7f0583
paolov85 <92166899+paolov85@users.noreply.github.com>
2023-11-30 19:03:08 +0100

    first function test

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,25 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "../scss/style.scss";
 
-console.log(process.env.API_URL);
-console.log(process.env.API_ID);
+window.addEventListener('load', () => {
+
+    const API_ITEM = process.env.API_ITEM;
+    const API_ASK= process.env.API_ASK;
+    const API_TOP= process.env.API_TOP;
+    const API_JOB= process.env.API_JOB;
+    const API_LATEST= process.env.API_LATEST;
+    const API_SHOW= process.env.API_SHOW;
+
+
+    getNews(API_LATEST)
+    
+
+    function getNews(api) {
+        axios
+            .get(api)
+            .then((response) => {
+                console.log(response.data);
+            })
+    }
+
+   });
