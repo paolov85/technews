e63463de673c70a1d7e9ec26fd5a051d7b5858f6
paolov85 <92166899+paolov85@users.noreply.github.com>
2023-12-06 19:40:17 +0100

    ui improvements

--- src/js/index.js
+++ src/js/index.js
@@ -8,23 +8,50 @@ import "../scss/style.scss";
 
 window.addEventListener('load', () => {
 
-    const API_ITEM = process.env.API_ITEM;
-    const API_ASK= process.env.API_ASK;
-    const API_TOP= process.env.API_TOP;
-    const API_JOB= process.env.API_JOB;
-    const API_LATEST= process.env.API_LATEST;
-    const API_SHOW= process.env.API_SHOW;
+    const API_ITEM = process.env.API_ITEM; //https://hacker-news.firebaseio.com/v0/item/
+    const API_ASK = process.env.API_ASK;
+    const API_TOP = process.env.API_TOP;
+    const API_JOB = process.env.API_JOB;
+    const API_LATEST = process.env.API_LATEST;
+    const API_SHOW = process.env.API_SHOW;
 
+    let newsIdList
+    let newsIdSmallList
+    getNewsIds(API_LATEST)
+    getItem(API_ITEM, newsIdSmallList)
 
-    getNews(API_LATEST)
-    
-
-    function getNews(api) {
+    function getNewsIds(apiUrl) {
         axios
-            .get(api)
-            .then((response) => {
-                console.log(response.data);
-            })
+        .get(apiUrl)
+        .then((response) => {
+            newsIdList = response.data;
+            getItem(API_ITEM, newsIdList)
+        })
     }
 
-   });
+    function getItem(apiUrl, newsIds) {
+        newsIdSmallList = newsIds.splice(0, 10)
+        newsIdSmallList.forEach(newsId => {
+            let itemUrl = `${apiUrl}${newsId}.json`
+            axios
+            .get(itemUrl)
+            .then((response) => {
+                console.log(response.data);
+                createCard()
+            })
+        });
+    }
+
+    function createCard () {
+        const cardContainer = document.querySelector('.main')
+        const cardWrap = document.createElement('div')
+        cardWrap.setAttribute('class', 'card mb-3')
+        cardContainer.appendChild(cardWrap)
+        const cardBody = document.createElement('div')
+        cardBody.setAttribute('class', 'card mb-3')
+    }   
+
+
+
+
+});
