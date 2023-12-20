1706a544ded8a2e2cd809f08e30420019b216624
paolov85 <92166899+paolov85@users.noreply.github.com>
2023-12-20 18:10:21 +0100

    working tabs

--- src/js/index.js
+++ src/js/index.js
@@ -6,103 +6,130 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "../scss/style.scss";
 
+const API_ITEM = process.env.API_ITEM; //https://hacker-news.firebaseio.com/v0/item/
+const API_ASK = process.env.API_ASK;
+const API_TOP = process.env.API_TOP;
+const API_JOB = process.env.API_JOB;
+const API_LATEST = process.env.API_LATEST;
+const API_SHOW = process.env.API_SHOW;
 
+let newsIdList
+let newsIdShortList
+let firstId
 
 window.addEventListener('load', () => {
 
-    document.getElementById('mainLoading').classList.add('d-none'); //controllare
-
-    const API_ITEM = process.env.API_ITEM; //https://hacker-news.firebaseio.com/v0/item/
-    const API_ASK = process.env.API_ASK;
-    const API_TOP = process.env.API_TOP;
-    const API_JOB = process.env.API_JOB;
-    const API_LATEST = process.env.API_LATEST;
-    const API_SHOW = process.env.API_SHOW;
-
-    let newsIdList
-    let newsIdShortList
-    let firstId = 0
-    let lastId = 10
     getNewsIds(API_LATEST)
-    getItem(API_ITEM, newsIdShortList)
+
+    document.getElementById('loadMoreBtn').onclick = () => {
+        document.getElementById('loading').classList.remove('d-none');
+        document.getElementById('loadMoreBtn').classList.add('d-none');
+        getCards(API_ITEM, newsIdList)
+        firstId += 10
+        console.log(firstId)
+    };
+
+    let urlId = ""
+    const clickedTab = document.querySelector('.nav');
+    clickedTab.onclick = (event) => {
+        const toDel = document.getElementById('main')
+        while (toDel.firstChild) {
+            toDel.removeChild(toDel.firstChild);
+        }
+        const target = event.target.closest('button');
+        console.log(target)
+        const inactive = document.querySelectorAll('.active')
+        inactive.forEach((inactiveEl) => {
+            inactiveEl.classList.remove('active')
+        })
+        target.classList.add('active')
+        target ? urlId = target.id : urlId = null;
+        console.log(urlId)
+        switch (urlId) {
+            case 'news':
+
+                getNewsIds(API_LATEST)
+                break;
+
+            case 'top':
+                getNewsIds(API_TOP)
+                break;
+
+            case 'ask':
+                getNewsIds(API_ASK)
+                break;
+
+            case 'job':
+                getNewsIds(API_JOB)
+                break;
+        }
+
+    }
+
 
     function getNewsIds(apiUrl) {
+        firstId = 0
         axios
             .get(apiUrl)
             .then((response) => {
                 newsIdList = response.data;
-                getItem(API_ITEM, newsIdList)
+                getCards(API_ITEM, newsIdList)
+                //verificare
+                document.getElementById('app').classList.remove('d-none');
             })
     }
 
-    function getItem(apiUrl, newsIds) {
+    function getCards(apiUrl, newsIds) {
 
-        newsIdShortList = newsIds.splice(firstId, lastId)
+        newsIdShortList = newsIds.splice(firstId, 10)
         newsIdShortList.forEach(newsId => {
             let itemUrl = `${apiUrl}${newsId}.json`
             axios
                 .get(itemUrl)
                 .then((response) => {
-                    console.log(response.data)
-                    createCard(response.data)
-                    document.getElementById('loading').classList.add('d-none');
-                    document.getElementById('loadMoreBtn').classList.remove('d-none');
-            
+
+                    const item = response.data
+                    const mainCard = document.getElementById('main')
+                    let date = dateConversion(item.time)
+                    let cardImg = ""
+                    switch (item.type) {
+                        case 'job':
+                            cardImg = 'job'
+                            break
+                        case 'story':
+                            cardImg = 'story'
+                            break
+                        case 'comment':
+                            cardImg = 'comment'
+                            break
+                        case 'poll':
+                            cardImg = 'poll'
+                            break
+                        case 'pollopt':
+                            cardImg = 'pollopt'
+                            break
+                    }
+
+                    const card = `
+                    <div class="card-warp card mb-3">
+                        <h5 class="card-header text-center">${date}</h5>
+                        <div class="card-body d-flex justify-content-around">
+                            <img class="card-logo col-3 mx-auto p-4" src="img/${cardImg}.svg" alt="img">
+                            <div class="card-main col-9 p-5">
+                                <h5 class="card-title">${item.title}</h5>
+                                <p class="card-text">by ${item.by}</p>
+                                <a href="${item.url}" class="btn btn-primary">read more</a>
+                            </div>
+                        </div>
+                    </div>
+                    `
+                    mainCard.insertAdjacentHTML("beforeend", card)
                 })
+
+            document.getElementById('loading').classList.add('d-none');
+            document.getElementById('loadMoreBtn').classList.remove('d-none');
         });
-        document.getElementById('loadMoreBtn').addEventListener('click', ()=>{
-            console.log(document.getElementById('loadMoreBtn').classList)
-            console.log(document.getElementById('loading').classList)
-            document.getElementById('loading').classList.remove('d-none');
-            document.getElementById('loadMoreBtn').classList.add('d-none');
-            firstId += 10
-            lastId += 10
-            console.log(firstId, lastId)
-            getItem(API_ITEM, newsIdList)
-        });
-
     }
-
-
-
-    function createCard(item) {
-        const mainCard = document.getElementById('main')
-        let date = dateConversion(item.time)
-        let cardImg = ""
-        switch (item.type) {
-            case 'job':
-                cardImg = 'job'
-                break
-            case 'story':
-                cardImg = 'story'
-                break
-            case 'comment':
-                cardImg = 'comment'
-                break
-            case 'poll':
-                cardImg = 'poll'
-                break
-            case 'pollopt':
-                cardImg = 'pollopt'
-                break
-        }
-
-        const card = `
-        <div class="card-warp card mb-3">
-            <h5 class="card-header text-center">${date}</h5>
-            <div class="card-body d-flex justify-content-around">
-                <img class="card-logo col-3 mx-auto p-4" src="img/${cardImg}.svg" alt="img">
-                <div class="card-main col-9 p-5">
-                    <h5 class="card-title">${item.title}</h5>
-                    <p class="card-text">by ${item.by}</p>
-                    <a href="${item.url}" class="btn btn-primary">read more</a>
-                </div>
-            </div>
-        </div>
-        `
-        mainCard.insertAdjacentHTML("beforeend", card)
-    }
-
 
     function dateConversion(unixTime) {
         let milliseconds = unixTime * 1000;
@@ -114,7 +141,7 @@ window.addEventListener('load', () => {
         var year = date.getFullYear();
         var hours = date.getHours();
         var minutes = date.getMinutes();
-        var time = 'h ' + (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
+        var time = '- ' + (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
         return day + ' ' + date.getDate() + ' ' + month + ' ' + year + ' ' + time;
     }
 
