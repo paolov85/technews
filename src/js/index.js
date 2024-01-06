9c4bb63451b7154e46909ae9f1d46a187d2cc0f9
paolov85 <92166899+paolov85@users.noreply.github.com>
2024-01-06 18:42:03 +0100

    no link bugfix

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-// const _ = require("lodash");
 const _get = require('lodash/get');
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -13,61 +12,72 @@ const API_JOB = process.env.API_JOB;
 const API_LATEST = process.env.API_LATEST;
 const API_SHOW = process.env.API_SHOW;
 
-let newsIdList
-let newsIdShortList
-let firstId
-let urlId = ""
+let newsIdList;
+let newsIdShortList;
+let firstId;
+let urlId = "";
 
 window.addEventListener('load', () => {
 
-    getNewsIds(API_LATEST)
-
+    getNewsIds(API_LATEST);
 
     function getNewsIds(apiUrl) {
         document.getElementById('loadMoreBtn').classList.add('d-none');
         document.getElementById('footer').classList.add('d-none');
-        firstId = 0
+        firstId = 0;
         axios
             .get(apiUrl)
             .then((response) => {
                 newsIdList = response.data;
-                getCards(API_ITEM, newsIdList)
+                getCards(API_ITEM, newsIdList);
+
             })
+            .catch(error => {
+                console.log('No response received:', error.request);
+                const errorModal = document.getElementById('errorModal');
+                errorModal.setAttribute('class', 'modal fade show');
+                errorModal.style.display = 'block';
+            });
     }
 
     function getCards(apiUrl, newsIds) {
-        newsIdShortList = newsIds.splice(firstId, 10)
+        newsIdShortList = newsIds.splice(firstId, 10);
         newsIdShortList.forEach(newsId => {
-            let itemUrl = `${apiUrl}${newsId}.json`
+            let itemUrl = `${apiUrl}${newsId}.json`;
             axios
                 .get(itemUrl)
                 .then((response) => {
-                    const item = response.data
-                    console.log(response.data)
-                    const mainCard = document.getElementById('main')
-                    const timeFixed = _get(response, 'data.time', 'unknow time')
-                    let date = dateConversion(timeFixed)
-                    let cardImg = item.type
+                    const item = response.data;
+                    const mainCard = document.getElementById('main');
+                    const timeFixed = _get(response, 'data.time', 'unknow time');
+                    const cardUrl = _get(response, 'data.url', 'no link');
+                    const date = dateConversion(timeFixed);
+                    const cardImg = item.type;
+                    const cardClasses = cardUrl === 'no link' ? 'btn btn-primary d-none' : 'btn btn-primary';
 
                     const card = `
-                    <div class="card-warp card mb-3">
-                        <h5 class="fw-bold card-header text-center">${date}</h5>
-                        <div class="card-body bgDark text-body d-flex justify-content-around">
-                            <img class="card-logo col-3 mx-auto p-4" src="img/${cardImg}.svg" alt="img">
-                            <div class="card-main col-9 p-5">
-                                <h5 class="card-title">${item.title}</h5>
-                                <p class="fst-italic card-text">by ${item.by}</p>
-                                <a href="${item.url}" target="blank" class="btn btn-primary">read more</a>
+                        <div class="card-warp card mb-3">
+                            <h5 class="fw-bold card-header text-center">${date}</h5>
+                            <div class="card-body bgDark text-body d-flex justify-content-around">
+                                <img class="card-logo col-3 mx-auto p-4" src="img/${cardImg}.svg" alt="img">
+                                <div class="card-main col-9 p-5">
+                                    <h5 class="card-title">${item.title}</h5>
+                                    <p class="fst-italic card-text">by ${item.by}</p>
+                                    <a href="${cardUrl}" target="blank" id="readmore" class="${cardClasses}">read more</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    `
-                    mainCard.insertAdjacentHTML("beforeend", card)
+                        `;
+                    mainCard.insertAdjacentHTML("beforeend", card);
                     document.getElementById('loading').classList.add('d-none');
                     document.getElementById('loadMoreBtn').classList.remove('d-none');
                     document.getElementById('footer').classList.remove('d-none');
                 })
-
+                .catch(error => {
+                    console.log('No response from id:', error);
+                    const errorModal = document.getElementById('errorModal');
+                    errorModal.setAttribute('class', 'modal fade show');
+                });
         });
     }
 
@@ -87,33 +97,32 @@ window.addEventListener('load', () => {
 
     const clickedTab = document.querySelector('.nav');
     clickedTab.onclick = (event) => {
-        const toDel = document.getElementById('main')
+        const toDel = document.getElementById('main');
         while (toDel.firstChild) {
             toDel.removeChild(toDel.firstChild);
         }
         const target = event.target.closest('button');
-        const inactive = document.querySelectorAll('.active')
+        const inactive = document.querySelectorAll('.active');
         inactive.forEach((inactiveEl) => {
-            inactiveEl.classList.remove('active')
+            inactiveEl.classList.remove('active');
         })
-        target.classList.add('active')
-        urlId = target.id
+        target.classList.add('active');
+        urlId = target.id;
         switch (urlId) {
             case 'news':
-
-                getNewsIds(API_LATEST)
+                getNewsIds(API_LATEST);
                 break;
 
             case 'top':
-                getNewsIds(API_TOP)
+                getNewsIds(API_TOP);
                 break;
 
             case 'ask':
-                getNewsIds(API_ASK)
+                getNewsIds(API_ASK);
                 break;
 
             case 'job':
-                getNewsIds(API_JOB)
+                getNewsIds(API_JOB);
                 break;
         }
     }
@@ -121,8 +130,19 @@ window.addEventListener('load', () => {
     document.getElementById('loadMoreBtn').onclick = () => {
         document.getElementById('loading').classList.remove('d-none');
         document.getElementById('loadMoreBtn').classList.add('d-none');
-        getCards(API_ITEM, newsIdList)
-        firstId += 10
+        getCards(API_ITEM, newsIdList);
+        firstId += 10;
     };
     document.getElementById('app').classList.remove('d-none');
+
+    const closeModalButtons = document.querySelectorAll('[data-bs-dismiss="modal"]');
+    closeModalButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            const modal = button.closest('.modal');
+            modal.setAttribute('class', 'modal fade');
+        });
+    });
+
+    const refreshBtn = document.getElementById('refresh');
+    refreshBtn.onclick = () => location.reload();
 });
